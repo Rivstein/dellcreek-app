33e923f774e55be1639cff6aec4d9e8349c6d0bb
Rivstein <51017849+Rivstein@users.noreply.github.com>
2022-06-05 06:36:56 +0300

    Test commit CLI : Bazenga

--- app/Http/Controllers/WebPagesController.php
+++ app/Http/Controllers/WebPagesController.php
@@ -10,7 +10,6 @@ class WebPagesController extends Controller
     //index
     public function index()
     {
-        // auth()->logout();
         $properties = Property::latest()->paginate(9);
         return view('landing',compact(
             'properties'
